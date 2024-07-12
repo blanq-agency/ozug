6d4981300e14caa199caa0b80370036fa62dc28b
Andrej Agapow <andrej@blanq.agency>
2024-07-12 18:45:14 +0200

    convert footer from `vue` to `antlers` template

--- sources/webapp/resources/js/app.js
+++ sources/webapp/resources/js/app.js
@@ -11,7 +11,6 @@ FloatingVue.options.themes.dropdown.placement = 'top'
 FloatingVue.options.themes.dropdown.distance = 10
 
 import AppHeader from '@/components/Layouts/AppHeader.vue'
-import AppFooter from '@/components/Layouts/AppFooter.vue'
 import Commentaries from '@/components/Pages/Commentaries.vue'
 import Commentary from '@/components/Pages/Commentary.vue'
 import Footnote from '@/components/Pages/Partials/Footnote.vue'
@@ -23,7 +22,6 @@ import VersionComparisonModalDialog from '@/components/Pages/Partials/VersionCom
 const app = createApp({
   components: {
     'app-header': AppHeader,
-    'app-footer': AppFooter,
     'commentaries': Commentaries,
     'commentary': Commentary,
     'footnote': Footnote,
@@ -37,7 +35,7 @@ const app = createApp({
 app.config.globalProperties.emitter = emitter
 app
   .use(FloatingVue)
-  .use(i18nVue, { 
-    resolve: (lang) => import(`../../lang/${lang}.json`) 
+  .use(i18nVue, {
+    resolve: (lang) => import(`../../lang/${lang}.json`)
   })
   .mount('#app')
