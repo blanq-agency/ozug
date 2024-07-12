6d7e7a34583f557c4cde06854fb10451b35e4957
Andrej Agapow <andrej@blanq.agency>
2024-07-12 19:25:25 +0200

    convert header from `vue` to `antlers` template

--- sources/webapp/resources/js/app.js
+++ sources/webapp/resources/js/app.js
@@ -10,7 +10,8 @@ import FloatingVue from 'floating-vue'
 FloatingVue.options.themes.dropdown.placement = 'top'
 FloatingVue.options.themes.dropdown.distance = 10
 
-import AppHeader from '@/components/Layouts/AppHeader.vue'
+import AppNav from '@/components/Layouts/AppNav.vue'
+import AppSidebar from '@/components/Layouts/AppSidebar.vue'
 import Commentaries from '@/components/Pages/Commentaries.vue'
 import Commentary from '@/components/Pages/Commentary.vue'
 import Footnote from '@/components/Pages/Partials/Footnote.vue'
@@ -21,7 +22,8 @@ import VersionComparisonModalDialog from '@/components/Pages/Partials/VersionCom
 
 const app = createApp({
   components: {
-    'app-header': AppHeader,
+    'app-nav': AppNav,
+    'app-sidebar': AppSidebar,
     'commentaries': Commentaries,
     'commentary': Commentary,
     'footnote': Footnote,
