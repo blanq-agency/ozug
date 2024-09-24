4b5cea0d3efdd424e533db10fcfa9619b50d3dd3
Andrej Agapow <andrej@blanq.agency>
2024-09-24 11:09:50 +0200

    feat: adjust control panel welcome widget text - #OZUG-132

--- sources/webapp/resources/views/widgets/welcome.blade.php
+++ sources/webapp/resources/views/widgets/welcome.blade.php
@@ -1,6 +1,6 @@
 <div class="card p-0 content">
   <div class="py-3 px-4 border-b">
-    <h1>Welcome to {{ config('app.name') }}</h1>
-    <p>Publishing platform for legal commentaries.</p>
+    <h1>{{ __('dashboard_welcome_widget_title_start') }} {{ config('app.name') }}</h1>
+    <p>{{ __('dashboard_welcome_widget_text') }}</p>
   </div>
 </div>
