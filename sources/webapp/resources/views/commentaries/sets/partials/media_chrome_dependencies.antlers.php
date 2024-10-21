234fb0715bdcfd89828c2cef5d8e940547ec7c8a
Andrej Agapow <andrej@blanq.agency>
2024-10-21 13:14:13 +0200

    media-chrome styling - #OZUG-17

--- sources/webapp/resources/views/commentaries/sets/partials/media_chrome_dependencies.antlers.php
+++ sources/webapp/resources/views/commentaries/sets/partials/media_chrome_dependencies.antlers.php
@@ -3,6 +3,12 @@
     <style>
       media-controller {
         --media-tooltip-display: none;
+        --media-primary-color: black;
+        --media-secondary-color: #f4f4f2;
+        --media-control-hover-background: hsl(60, 8%, 90%);
+
+        --media-range-track-background: white;
+        --media-preview-time-text-shadow: none;
       }
     </style>
   {{ /push:styles }}
