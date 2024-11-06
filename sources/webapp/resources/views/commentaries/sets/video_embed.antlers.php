491feacc70afb573c5226d9da58abe33aeb9f702
Andrej Agapow <andrej@blanq.agency>
2024-11-06 18:27:37 +0100

    feat: wikimedia commons video embed - #OZUG-147

--- sources/webapp/resources/views/commentaries/sets/video_embed.antlers.php
+++ sources/webapp/resources/views/commentaries/sets/video_embed.antlers.php
@@ -1,13 +1,28 @@
 {{ partial src="commentaries/sets/partials/media_chrome_dependencies" }}
 
 <figure class="mx-auto w-full {{ width_limit ? 'max-w-[476px]' : null }}">
-  {{ if video | contains("youtube.com/watch") or video | contains('vimeo.com/') }}
+  {{ if
+    video | contains("youtube.com/watch")
+    or video | contains('vimeo.com/')
+    or video | contains('commons.wikimedia.org/wiki/')
+  }}
     <div class="relative w-full aspect-video" data-oak-marketing-cookies-consent>
-      <iframe
-        data-src="{{ video | embed_url }}"
-        frameborder="0"
-        class="w-full h-full"
-      ></iframe>
+      {{ if
+        video | contains("youtube.com/watch")
+        or video | contains('vimeo.com/')
+      }}
+        <iframe
+          data-src="{{ video | embed_url }}"
+          frameborder="0"
+          class="w-full h-full"
+        ></iframe>
+      {{ else video | contains('commons.wikimedia.org/wiki/') }}
+        <iframe
+          data-src="{{ video + ((video | contains('?')) ? '&embedplayer=yes' : '?embedplayer=yes') }}"
+          frameborder="0"
+          class="w-full h-full"
+        ></iframe>
+      {{ /if }}
       <div class="absolute inset-0 grid place-items-center bg-ok-beige" data-oak-marketing-cookies-consent-banner>
         <div class="no-prose text-center p-4">
           <p class="">Marketing-Cookies akzeptieren um dieses Video anzuzeigen.</p>
