36c08df26bba26b59a15c8973cb4e6546b73d771
Jack Sleight <jacksleight@gmail.com>
2026-06-01 17:33:47 +0100

    Add link field to image sets and constrain grid image width

--- sources/webapp/resources/views/commentaries/sets/image_embed.antlers.php
+++ sources/webapp/resources/views/commentaries/sets/image_embed.antlers.php
@@ -1,11 +1,13 @@
-<figure class="mx-auto {{ width_limit ? 'max-w-[476px]' : null }}">
-  {{ image }}
-    <img
-      src="{{ url }}"
-      alt="{{ alt }}"
-      width="{{ width }}"
-      height="{{ height }}"
-    />
-  {{ /image }}
+<figure class="mx-auto min-w-0 {{ width_limit ? 'max-w-[476px]' : 'max-w-full' }}">
+  {{ if link }}<a href="{{ link }}" target="_blank" rel="noopener">{{ /if }}
+    {{ image }}
+      <img
+        src="{{ url }}"
+        alt="{{ alt }}"
+        width="{{ width }}"
+        height="{{ height }}"
+      />
+    {{ /image }}
+  {{ if link }}</a>{{ /if }}
   {{ partial src="commentaries/sets/partials/caption" }}
 </figure>
